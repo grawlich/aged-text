51a7f933375a8ece687e4b10c18eaf5ed454a210
Kevin McKraken <kevin@grawlich.art>
2026-07-28 02:19:04 -0400

    Style updates for demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	// Components
-  import AgedText from '../components/AgedText.svelte'
+  import AgedText from '../components/AgedText.svelte';
 
 
   // Sample data (for testing)
-  const data = [
+  let data = [
     {
       id: 'liajfdsa09453afs',
       post_body: `Holy cow, this post seems to be fading away!`,
@@ -25,30 +25,75 @@
       post_body: `Holy cow, this post seems to be fading away!`,
       date_created: `2026-07-25T21:22:00.000Z`,
     },
-  ]
+  ];
+
+
+  // Settings for demo
+  const randomizeOptions = [
+    {
+      label: "Letters",
+      value: "letters",
+    },
+    {
+      label: "Words",
+      value: "words",
+    },
+    {
+      label: "All",
+      value: "all",
+    },
+  ];
+
+  
+  let randomize = $state("letters");
+  let start = $state(100);
+  let end = $state(10);
+  let offset = $state(25);
+  let text = $state("Holy cow, this post seems to be fading away!");
+  let date_1 = $state();
+  let date_2 = $state();
+  let date_3 = $state();
+  let date_4 = $state();
+  let missingText = $state("?");
+  let missingOpacity = $state(25);
+  let lifespan = $state(60000 * 100000);
+  let refreshInterval = $state(10000);
+
+
+  function applyDemoText(text) {
+    data.forEach((item)=>{
+      item.post_body = text;
+    });
+  }
 </script>
+
+
+
 
 
 <!-- Post list -->
 <ul class="post-list">
   <!-- Post cards -->
-  {#each data as post}
+  {#each data.reverse() as post, i}
     <li class="post-card">
       <!-- Aging text item -->
       <AgedText
         data={post}
         class="post-text"
-        randomize="letters"
-        start={100}
-        end={0}
-        offset={25}
+        {randomize}
+        {start}
+        {end}
+        {offset}
         text="post_body"
         date="date_created"
-        missingText="?"
-        missingOpacity={50}
-        lifespan={60000 * 100000}
-        refreshInterval={10000}
+        {missingText}
+        {missingOpacity}
+        {lifespan}
+        {refreshInterval}
       />
+
+      <!-- Date posted -->
+      <p class="post-date">{new Date(post?.date_created).toLocaleDateString()}</p>
     </li>
   {/each}
 </ul>
@@ -59,12 +104,14 @@
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: calc(var(--spacing) * 4);
+    gap: calc(var(--spacing) * 2);
   }
 
 
   .post-card {
-    width: fit-content;
+    position: relative;
+    width: 100%;
+    max-width: 65ch;
     padding: calc(var(--spacing) * 4);
     background-color: light-dark(var(--color-surface-950), var(--color-surface-50));
     color: light-dark(var(--color-surface-contrast-950), var(--color-surface-contrast-50));
@@ -72,19 +119,38 @@
     display: flex;
     border-radius: calc(var(--spacing) * 3);
     flex-direction: column;
-    align-items: center;
-    gap: calc(var(--spacing) * 8);
+    gap: calc(var(--spacing) * 2);
     box-shadow: 0 0 0 0 transparent;
-    transition: 150ms ease all;
+    transition: 200ms ease all;
+
+    &::after {
+      position: absolute;
+      z-index: -1;
+      content: "";
+      inset: 0;
+      border-color: var(--color-primary-500);
+      box-sizing: border-box !important;
+    }
+
 
     &:hover {
-      transform: scale(1.05);
-      box-shadow: 0 .5rem 0 color-mix(in oklch, var(--color-black) 25%, transparent);
+      transform: scale(1.025);
+      box-shadow: 0 1rem 0 color-mix(in oklch, var(--color-black) 25%, transparent);
+      
+      &::after {
+        border-width: 3px;
+        transition: inherit;
+      }
     }
     
     @supports (corner-shape: squircle) {
       corner-shape: squircle;
       border-radius: 100vh;
+      
+      &::after {
+        corner-shape: inherit;
+        border-radius: inherit;
+      }
     }
   }
 
@@ -92,6 +158,51 @@
 	/* Non-Tailwind classes must be in the global scope */
 	/* You could also add this to the `app.css` file without `:global()` instead */
   :global(.post-text) {
+    font-size: var(--text-lg);
     line-height: 1;
+  }
+
+
+  .post-date {
+    width: 100%;
+    font-size: var(--text-xs);
+    line-height: 1;
+    text-align: right;
+  }
+
+
+  .settings-container {
+    max-width: 65ch;
+    margin: 0 auto;
+    margin-bottom: calc(var(--spacing) * 4);
+    padding: calc(var(--spacing) * 4);
+    padding-top: calc(var(--spacing) * 2);
+    border-width: 1px;
+    border-radius: .75rem;
+  }
+
+
+  .settings-title {
+    font-weight: 600;
+    padding-inline: calc(var(--spacing) * 2);
+  }
+
+
+  .setting-wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+
+
+  .setting-label {
+    font-weight: 500;
+    font-size: var(--text-sm);
+  }
+
+
+  .setting-select {
+    width: fit-content;
+    border-width: 1px;
+    border-radius: .5rem;
   }
 </style>
